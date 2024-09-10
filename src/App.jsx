cd69558d8d5936ab1f1583c70f5839a0dbd2dc5d
Jawidan77 <jawidan.reza77@gmail.com>
2024-09-10 10:57:39 -0400

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './component/Header.jsx'
 import { Blogpost1, Blogpost2 } from './component/Blogpost.jsx'
-import { Footer } from './component/footer.jsx'
+import { Footer } from './component/Footer.jsx'
 
 import './App.css'
 
